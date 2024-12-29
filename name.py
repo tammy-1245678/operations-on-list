fcfffa9b49cf40a28f1c799a615b415a74dad8be
tammy-245678 <abdullahbigman38@gmail.com>
2024-12-29 21:05:27 +0500

    commit

---
+++ name.py
@@ -0,0 +1,19 @@
+fruitlist=['apple','guava','mango','banana','kivi']
+print("length of list:",len(fruitlist))
+print("first element:",fruitlist[3])
+print("frint last element:",fruitlist[-1])
+fruitlist.append('papaya')
+print("updated list:",fruitlist)
+fruitlist.remove('guava')
+print("updated list:",fruitlist)
+fruitlist.sort()
+print("sortedlist:,fruitlist")
+poppeditem=fruitlist.pop(1)
+print("my popped item",poppeditem)
+fruitlist.reversed()
+print("reversedlist:",fruitlist)
+print(fruitlist)
+
+
+
+
